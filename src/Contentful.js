3b269b4554396868e6cceb7733af4dae5276a416
sagar2809 <50287208+sagar2809@users.noreply.github.com>
2021-05-18 21:13:35 +0530

    enviroment variable changed

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,9 +1,7 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-   space:"p1i4xpykmljk",
- //space: process.env.REACT_APP_API_SPACE,
-   accessToken:"P-B64Z_ZYKJyg54NE4OoFa2-E3XNFJGZUWTxKB_XE9c"
-   // claccessToken : process.env.REACT_APP_ACCESS_TOKEN
+ space: process.env.REACT_APP_API_SPACE,
+   accessToken : process.env.REACT_APP_ACCESS_TOKEN
 
 })
